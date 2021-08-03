9ec88aad94f1be8788af6b1bbe1d81b3709c1313
Erik Wenske <es.wenske@t-online.de>
2021-08-03 19:20:29 +0200

    Added logging function, have to add format later

--- app/src/main/java/de/test/errorcorrection/MainActivity.kt
+++ app/src/main/java/de/test/errorcorrection/MainActivity.kt
@@ -1,16 +1,26 @@
 package de.test.errorcorrection
 
+import android.Manifest
 import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.PrintWriter
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -31,6 +41,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Checking permissions
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1);
+        }
+        setContentView(R.layout.activity_main);
+
         //get reference for tts button
         val btn_tts = findViewById<Button>(R.id.playButton)
 
@@ -41,8 +68,13 @@ class MainActivity : AppCompatActivity() {
             if (text.isNotEmpty()) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     textToSpeechEngine.speak(text, TextToSpeech.QUEUE_FLUSH, null, "tts1")
+
+                    readLog()
                 } else {
                     textToSpeechEngine.speak(text, TextToSpeech.QUEUE_FLUSH, null)
+
+                    readLog()
+
                 }
             } else {
                 Toast.makeText(this, "Text cannot be empty", Toast.LENGTH_LONG).show()
@@ -57,7 +89,7 @@ class MainActivity : AppCompatActivity() {
                 RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
             )
-            sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            sttIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.GERMAN)
             sttIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now!")
 
             try {
@@ -82,6 +114,7 @@ class MainActivity : AppCompatActivity() {
                     result?.let {
                         val recognizedText = it[0]
                         textbox.setText(recognizedText)
+                        writeLog(recognizedText)
                     }
                 }
             }
@@ -96,5 +129,46 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         textToSpeechEngine.shutdown()
         super.onDestroy()
+    }
+
+    //TODO add string type
+    private fun writeLog(text: String) {
+
+        val sd_main = File(Environment.getExternalStorageDirectory(), "test")
+        var success = true
+        if (!sd_main.exists())
+            success = sd_main.mkdir()
+
+        if (success) {
+            //directory exists or already created
+            val sd = File("voice_log.txt")
+            Log.e("DEBUG","Ordner erstellt/existiert")
+            val dest = File(sd_main, "voice_log.txt")
+            println("Pfad: " + dest.path)
+            try {
+                dest.appendText(text + "\n")
+            } catch (e: Exception) {
+                //TODO handle exception
+            }
+        } else {
+            //directory creation is not successful
+            Log.e("DEBUG", "Erstellen vom Odner fehlgeschlagen")
+        }
+    }
+
+    private fun readLog() {
+        val sd_main = File(Environment.getExternalStorageDirectory(), "test")
+        var success = true
+        if (!sd_main.exists())
+            success = sd_main.mkdir()
+
+        if (success) {
+            val sd = File(sd_main, "voice_log.txt")
+            println("Path in read: " + sd.path)
+            val lineList = mutableListOf<String>()
+            sd.useLines { lines -> lines.forEach { lineList.add(it) } }
+            lineList.forEach {println("DEBUG: " + it)}
+
+        }
     }
 }
